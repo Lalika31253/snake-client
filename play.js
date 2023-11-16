870dae1faa52c1a886f4fb0338d1335bdc0fd2fe
Lalika31253 <Lalika31253@gmail.com>
2023-11-15 20:08:37 -0700

    Add client.js file and move the connection logic into it

--- play.js
+++ play.js
@@ -1,22 +1,13 @@
-const net = require("net");
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
+const conn = connect();
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //handle incoming data from server
+//handle incoming data from server
   conn.on("data", () => {
     console.log("You ded cuz yoy idled!");
   });
 
-  return conn;
-};
+  
 
-console.log("Connecting ...");
-connect();
+
+  
