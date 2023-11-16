92c1adc411d648d92e69d7304562f8a25c76e88b
Lalika31253 <Lalika31253@gmail.com>
2023-11-15 22:44:14 -0700

    Implement function with move signals to the game server

--- client.js
+++ client.js
@@ -12,11 +12,29 @@ const connect = function () {
 
   //print a message to the screen when the connection is successfully established
   conn.on("connect", () => {
-    ;
     console.log("Successfully connected to game server!");
     conn.write("Name: PAM"); // apply name to the snake
   });
 
+  //create moves array to store different moves
+  const moves = [
+    "Move: up",
+    "Move: down",
+    "Move: left",
+    "Move: right"
+  ];
+
+  conn.on("connect", () => {
+    let index = 0;
+    const delayMoves = setInterval(() => {
+      if (index < moves.length) {
+        conn.write(moves[index]);
+        index++;
+      } else {
+        clearInterval(delayMoves);
+      }
+    }, 500);
+  });
 
   return conn;
 };
