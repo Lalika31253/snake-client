27ef8c6c0908a680f692d1e9b147eb162b235346
Lalika31253 <Lalika31253@gmail.com>
2023-11-17 11:36:37 -0700

    Add handleUserInput function to handle CTRL+C user input

--- play.js
+++ play.js
@@ -3,11 +3,32 @@ const connect = require('./client');
 const conn = connect();
 
 //handle incoming data from server
-  conn.on("data", () => {
-    console.log("You ded cuz yoy idled!");
+conn.on("data", () => {
+  console.log("You ded cuz yoy idled!");
+});
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+const handleUserInput = function () {
+
+  process.stdin.on("data", (key) => {
+    
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    };
+
   });
+};
 
-  
-
-
-  
+setupInput();
